10e9f85293cdaa3f9d916e5aceb3f3adc3d8e83a
aravind-2002-1 <85395786+aravind-2002-1@users.noreply.github.com>
2023-05-26 09:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,43 @@
-from flask import Flask, render_template, request, send_file
-from werkzeug.utils import secure_filename
-import os
-import ffmpeg
-import requests
+from flask import Flask, render_template, request
+import base64
 
 app = Flask(__name__)
-app.config['CONVERTED_FOLDER'] = 'converted'
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/convert', methods=['POST'])
-def convert():
-    # Get the video URL from the form
-    video_url = request.form.get('video_url')
+@app.route('/encode_decode', methods=['POST'])
+def encode_decode():
+    key = request.form['private_key']
+    message = request.form['message']
+    mode = request.form['mode']
+    
+    if mode == 'e':
+        result = encode(key, message)
+    elif mode == 'd':
+        result = decode(key, message)
+    else:
+        result = 'Invalid Mode'
+    
+    return render_template('result.html', result=result)
 
-    # Check if a video URL was provided
-    if not video_url:
-        return render_template('index.html', message='No video URL provided')
+def encode(key, message):
+    enc=[]
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+    
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
-    # Download the video from the URL
-    response = requests.get(video_url)
-    if response.status_code != 200:
-        return render_template('index.html', message='Failed to download video from the provided URL')
-
-    # Save the downloaded video to a file
-    filename = secure_filename('video.mp4')
-    video_path = os.path.join(app.config['CONVERTED_FOLDER'], filename)
-    with open(video_path, 'wb') as file:
-        file.write(response.content)
-
-    # Convert the video to 480p
-    converted_filename = f"converted_{filename}"
-    converted_path = os.path.join(app.config['CONVERTED_FOLDER'], converted_filename)
-    ffmpeg.input(video_path).output(converted_path, vf='scale=854:480').run()
-
-    # Provide the download link to the converted video
-    return render_template('index.html', converted_file=converted_filename)
-
-@app.route('/download/<path:filename>')
-def download(filename):
-    converted_path = os.path.join(app.config['CONVERTED_FOLDER'], filename)
-
-    # Check if the file exists
-    if not os.path.isfile(converted_path):
-        return render_template('index.html', message='File not found')
-
-    # Send the file for download
-    return send_file(converted_path, as_attachment=True)
+def decode(key, message):
+    dec=[]
+    message = base64.urlsafe_b64decode(message).decode()
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        dec.append(chr((256 + ord(message[i])- ord(key_c)) % 256))
+    
+    return "".join(dec)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
